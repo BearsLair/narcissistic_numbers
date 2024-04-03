f8f0e45d48e0b5ec4acd764a8d6dbf62ec7f64aa
Patrick Gross <bears_lair@protonmail.com>
2024-04-02 22:07:47 -0700

    number converted to array of numbers

---
+++ narcissistic.js
@@ -0,0 +1,8 @@
+function narcissistic(value) {
+  console.log(value);
+  const numArray = Array.from(String(value), Number);
+  numArray.map((num) => )
+    return numArray;
+}
+
+console.log(narcissistic(153));
